cacc8693850352c7e8c55ced1977f6fd9abfbe74
Chris <chrisli2017@gmail.com>
2019-01-17 12:32:03 -0500

    deploy

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const app = require('./server')
-const PORT = process.env.PORT || 8080heroku 
+const PORT = process.env.PORT || 8080
 
 
 app.listen(PORT, () => console.log(`studiously serving silly sounds on port ${PORT}`))
